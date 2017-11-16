1a3b1075b0a5663f27545530f4e9c91a6541632f
Ashish Jain (NUGET) <asja@microsoft.com>
2017-11-15 20:01:35 -0800

    Updated running single restore for all the packages being installed as PackageReference Also fixed restoring parent projects when child project is upgraded from pc to pr Fixed error handing

--- src/NuGet.Clients/NuGet.Tools/NuGetPackage.cs
+++ src/NuGet.Clients/NuGet.Tools/NuGetPackage.cs
@@ -544,7 +544,7 @@ namespace NuGetVSExtension
             var settings = uiController.UIContext.UserSettingsManager.GetSettings(GetProjectSettingsKey(nuGetProject));
             var collapseDependencies = settings.NuGetProjectUpgradeCollapseDependencies;
 
-            collapseDependencies = await UIActionEngine.UpgradeNuGetProjectAsync(uiController, nuGetProject,
+            collapseDependencies = await uiController.UIContext.UIActionEngine.UpgradeNuGetProjectAsync(uiController, nuGetProject,
                 collapseDependencies);
 
             settings.NuGetProjectUpgradeCollapseDependencies = collapseDependencies;
